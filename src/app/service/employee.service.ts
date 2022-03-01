d9a2c5cdcfec01a7e8a1cf36c609a3e580421f7d
Witold <gasowski.witold@gmail.com>
2022-03-01 23:50:03 +0100

    service method

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -17,6 +17,14 @@ export class EmployeeService{
     }
 
     public addEmployee(employee: Employee): Observable<Employee> {
-      return this.http.get<Employee>(`${this.apiServerUrl}/employee/add`)
+      return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+    }
+
+    public updateEmployee(employee: Employee): Observable<Employee> {
+      return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+    }
+
+    public deleteEmployee(employeeId: number): Observable<void> {
+      return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
     }
 }
